348a684c36e1b5b3e089d50e649c125119e31c8e
Baggiyam Shanmugam <gowribaggiyam@gmail.com>
2025-02-03 16:59:25 +0100

    solved lab with bonous

--- index.js
+++ index.js
@@ -1,17 +1,59 @@
 class SortedList {
-  constructor() {}
+  constructor(itemArray = []) {
+    // Sort the array and assign it to 'items'
+    this.items = itemArray.sort((a, b) => a - b); // Sorting the array
+    this.length = this.items.length; // Assign length after sorting
+  }
 
-  add(item) {}
+  // Method to display the items and their length
+  displayItems() {
+    console.log("Items:", this.items);
+    console.log("Length:", this.length);
+  }
 
-  get(pos) {}
+  // Method to add a new item to the list
+  add(item) {
+    this.items.push(item); // Add the item to the array
+    this.items.sort((a, b) => a - b); // Sort the array after adding the item
+    this.length = this.items.length; // Update the length
+  }
 
-  max() {}
+  // Method to get the item at a specific position
+  get(pos) {
+    if (pos < 0 || pos >= this.length) {
+      throw new Error("Out of bounds"); // Error if the position is out of range
+    }
+    return this.items[pos]; // Return the item at the given position
+  }
 
-  min() {}
+  // Method to get the maximum value
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList"); // Error if the list is empty
+    }
+    return this.items[this.length - 1]; // Return the last item (max value)
+  }
 
-  sum() {}
+  // Method to get the minimum value
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList"); // Error if the list is empty
+    }
+    return this.items[0]; // Return the first item (min value)
+  }
 
-  avg() {}
+  // Method to get the sum of all the items
+  sum() {
+    return this.items.reduce((acc, val) => acc + val, 0); // Sum all elements in the array
+  }
+
+  // Method to get the average of the items
+  avg() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList"); // Error if the list is empty
+    }
+    return this.sum() / this.length; // Calculate average by dividing sum by length
+  }
 }
 
 module.exports = SortedList;
